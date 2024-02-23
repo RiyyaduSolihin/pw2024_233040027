0345dd18512c7029b80b58a0626ddb676f4c4a34
RiyyaduSolihin <riyyadu1234@gmail.com>
2024-02-23 11:29:47 +0700

    first commit

---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,7 @@
+<?php
+$nama = "Riyyadu";
+$umur = "20";
+$univ = "Universitas Pasundan";
+
+
+echo " Halo, nama saya $nama Umur saya $umur tahun saya kuliah di $univ";
---
+++ tugas/tugas1/tugas1b.php
@@ -0,0 +1,12 @@
+<?php
+ $angka = 27;
+    echo "Aku adalah angka <b> $angka";
+    echo "</b><br>";
+    echo "Jika aku dikali 5, maka aku sekarang menjadi <b>" . ( $angka * 5);
+    echo "</b><br>";
+    echo "jika aku dibagi 2, maka aku sekarang akan menjadi <b>" . ($angka * 5 / 2);
+    echo "</b></br>";
+    echo "jika aku ditambah 75, maka aku sekarang menjadi <b>" . ($angka * 5 / 2 + 75);
+    echo "</b><br>";
+    echo "jika aku dikurang 20, maka aku sekarang menjadi <b>" . ($angka * 5 / 2 + 75 - 20);
+    
