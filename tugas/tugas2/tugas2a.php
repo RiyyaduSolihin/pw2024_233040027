fb4cae6db9dffba15c99271133ef0ee3b09930f9
RiyyaduSolihin <riyyadu1234@gmail.com>
2024-02-24 20:09:12 +0700

    menambahkan

--- tugas/tugas2/tugas2a.php
+++ tugas/tugas2/tugas2a.php
@@ -1,1 +1,24 @@
 <?php
+
+echo "<br>";
+$namadepan = "Riyyadu";
+$namabelakang = "Solihin";
+ 
+for( $i= 1; $i <= 100; $i++) {
+    if ($i % 3 === 0 && $i % 5 === 0) {
+       echo "$namadepan $namabelakang<br>";
+       
+
+    } elseif ($i % 3 === 0 ) {
+        echo "$namadepan <br>";
+
+     } elseif ($i % 5 === 0) {
+        echo "$namabelakang<br>";
+
+     } else {
+        echo "$i <br>";
+     }
+    
+        
+      
+}
---
+++ tugas/tugas2/tugas2b.php
@@ -0,0 +1,9 @@
+<?php
+function pascal ($n, $r) {
+    if
+}
+
+for( $i= 1; $i <= 100; $i++) {
+    echo "Hello World $i x! <br>";
+    
+}
