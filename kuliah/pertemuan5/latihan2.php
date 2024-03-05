d43945ec28139aa24f852b7c030b86a860eb917c
RiyyaduSolihin <riyyadu1234@gmail.com>
2024-03-05 15:20:10 +0700

    menambahkan materi

---
+++ kuliah/pertemuan5/latihan2.php
@@ -0,0 +1,50 @@
+<?php
+$binatang = ["😼", "🐰", "🐬", "🐶", "🐺", "🐊"];
+$makanan = ["🍜","🍫","🍙","🍚","🥘"]
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Array</title>
+</head>
+<body>
+    <h3>Daftar Binatang</h3>
+    <ol>
+        <?php for($i = 0; $i < count($binatang); $i++) { ?>
+        <li><?php echo $binatang[$i]; ?></li>
+        <?php } ?>
+    </ol>
+
+    <h3>Daftar Makanan</h3>
+    <ul>
+    <?php for($i = 0; $i < count($makanan); $i++) { ?>
+        <li><?php echo $makanan[$i]; ?></li>
+        <?php } ?>
+    </ul>
+
+    <hr>
+
+    <h3>Daftar Binatang</h3>
+    <ol>
+        <?php foreach($binatang as $b ) : ?>
+        <li><?= $b; ?></li>
+        <?php endforeach; ?>
+    </ol>
+
+    <hr>
+
+    <h3>Daftar Makanan</h3>
+    <ol>
+        <?php foreach($makanan as $b ) : ?>
+        <li><?=$b; ?></li>
+        <?php endforeach; ?>
+    </ol>
+    
+
+    
+
+
+</body>
+</html>
